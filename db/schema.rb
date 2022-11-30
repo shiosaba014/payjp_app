9646bd5769fbcd08b2aaa48d99db808bacfb6019
shiosaba014 <shiosaba014@gmail.com>
2022-11-30 19:36:54 +0900

    Revert "jsファイルで環境変数を読み込めるように"
    This reverts commit 91c24fd9fecd4b9113dee446fcc115822f5c2036.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_102505) do
-
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "customer_token", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_cards_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2022_11_30_092517) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -40,5 +32,4 @@ ActiveRecord::Schema.define(version: 2022_11_30_102505) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "cards", "users"
 end
--- test/controllers/cards_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get cards_new_url
-    assert_response :success
-  end
-
-end
