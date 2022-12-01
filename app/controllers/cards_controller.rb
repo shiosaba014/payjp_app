7497366ff8fbc8a2c437c2d7d72e41b2b2a76288
shiosaba014 <shiosaba014@gmail.com>
2022-12-01 18:50:07 +0900

    バリデーションの設定

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,8 +8,14 @@ class CardsController < ApplicationController
       card: params[:card_token] # 登録しようとしているカード情報
     )
     card = Card.new( # 顧客トークンとログインしているユーザーを紐付けるインスタンスを生成
+      card_token: params[:card_token],
       customer_token: customer.id, # 顧客トークン
       user_id: current_user.id # ログインしているユーザー
     )
+    if card.save
+      redirect_to root_path
+    else
+      redirect_to action: "new" # カード登録画面へリダイレクト
+    end
   end
 end
