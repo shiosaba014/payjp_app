ce7767fae8a18698f6dc40841eb1c3db1dab8c59
shiosaba014 <shiosaba014@gmail.com>
2022-12-07 13:09:41 +0900

    コントローラーの記述の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: :order
   def index
     @items = Item.all
+  end
+
+  def order
+    redirect_to new_card_path and return unless current_user.card.present?
+
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
+    customer_token = current_user.card.customer_token # ログインしているユーザーの顧客トークンを定義
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      customer: customer_token, # 顧客のトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
+    ItemOrder.create(item_id: params[:id])
+
+    redirect_to root_path
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
